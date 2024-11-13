dcc919281784d6eec96b4755fe913f7c6d4f4dd1
DUSTIN <elmerhomero66a@gmail.com>
2024-11-13 10:02:43 -0400

    Creando el repositorio del proyecto

---
+++ src/main/java/com/desafioLiterAlura/desafioLiterAlura/DesafioLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.desafioLiterAlura.desafioLiterAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiterAluraApplication.class, args);
+	}
+
+}
